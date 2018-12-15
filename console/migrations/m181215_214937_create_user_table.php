6dda0166632d226b7fee4b4f795bc94152cb7ce4
vagrant <vagrant@y2aa>
2018-12-15 23:22:33 +0000

    commonmodels

---
+++ console/migrations/m181215_214937_create_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `user`.
+ */
+class m181215_214937_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('user', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'email' => $this->string()->defaultValue(null),
+            'password' => $this->string(),
+            'role' => $this->string()->defaultValue('User'),
+            'photo' => $this->string()->defaultValue(null),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('user');
+    }
+}
